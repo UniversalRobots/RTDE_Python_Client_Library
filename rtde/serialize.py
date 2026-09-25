bb55c7b2ff89039f48a43880ba717fc04de3abe4
michal-milkowski <mmi@universal-robots.com>
2026-09-25 11:20:23 +0000

    Decoding branches in  unpack_field consolidated

--- rtde/serialize.py
+++ rtde/serialize.py
@@ -126,15 +126,11 @@ def unpack_field(data, offset, data_type):
     size = get_item_size(data_type)
     if data_type in ("VECTOR6D", "VECTOR3D"):
         return [float(data[offset + i]) for i in range(size)]
-    elif data_type == "VECTOR6UINT32":
+    elif data_type in ("VECTOR6UINT32", "VECTOR6INT32"):
         return [int(data[offset + i]) for i in range(size)]
     elif data_type == "DOUBLE":
         return float(data[offset])
-    elif data_type == "UINT32" or data_type == "UINT64":
-        return int(data[offset])
-    elif data_type == "VECTOR6INT32":
-        return [int(data[offset + i]) for i in range(size)]
-    elif data_type == "INT32" or data_type == "UINT8":
+    elif data_type in ("INT32", "UINT8", "UINT32", "UINT64"):
         return int(data[offset])
     elif data_type == "BOOL":
         return bool(data[offset])
